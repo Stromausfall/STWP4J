78e070c3c4fe03ca4b41d6fef28c0d703f372263
Matthias <mail@matthias-auer.net>
2015-12-17 22:13:57 +0100

    implemented a first draft of the scheduler

--- src/net/matthiasauer/stwp4j/Scheduler.java
+++ src/net/matthiasauer/stwp4j/Scheduler.java
@@ -1,11 +1,11 @@
 package net.matthiasauer.stwp4j;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 import java.util.logging.Logger;
 
@@ -13,11 +13,9 @@ public class Scheduler {
     private static final Logger logger = Logger.getLogger(Scheduler.class.getName());
     private static int instanceCounter = 0;
     private final int id = (instanceCounter++);
-    private Set<LightweightProcess> processes =
-            new HashSet<LightweightProcess>();
-    private Map<String, SchedulerChannel<?>> channels =
-            new HashMap<String, SchedulerChannel<?>>();
-    
+    private Set<LightweightProcess> processes = new HashSet<LightweightProcess>();
+    private Map<String, SchedulerChannel<?>> channels = new HashMap<String, SchedulerChannel<?>>();
+
     private void throwError(String message) {
         logger.severe(message);
         throw new IllegalArgumentException(message);
@@ -28,120 +26,99 @@ public class Scheduler {
             throwError(this.id + " | process already added to the scheduler, process : " + lightweightProcess);
         }
 
-        logger.info(this.id + " | added lightweightProcess to scheduler : " + lightweightProcess);;
+        logger.info(this.id + " | added lightweightProcess to scheduler : " + lightweightProcess);
+        ;
         this.processes.add(lightweightProcess);
-        
+
         this.createOutputChannels(lightweightProcess);
     }
 
     @SuppressWarnings({ "unchecked", "rawtypes" })
     private SchedulerChannel<?> getSchedulerChannelEntry(String channelId, Class<?> channelType) {
         SchedulerChannel<?> entry = this.channels.get(channelId);
-        
+
         if (entry == null) {
             entry = new SchedulerChannel(channelId, channelType);
-            
+
             logger.info(this.id + " | created a channel : " + channelId);
-            
+
             this.channels.put(channelId, entry);
         }
-        
+
         return entry;
     }
-    
+
     private void createOutputChannels(LightweightProcess lightweightProcess) {
-        List<ChannelPortsRequest<?>> channelRequests =
-                lightweightProcess.getChannelRequests();
-        Map<String, ChannelOutPort<?>> outputChannels =
-                new HashMap<String, ChannelOutPort<?>>();
-        Map<String, ChannelInPort<?>> inputChannels =
-                new HashMap<String, ChannelInPort<?>>();
-        
+        List<ChannelPortsRequest<?>> channelRequests = lightweightProcess.getChannelRequests();
+        Map<String, ChannelOutPort<?>> outputChannels = new HashMap<String, ChannelOutPort<?>>();
+        Map<String, ChannelInPort<?>> inputChannels = new HashMap<String, ChannelInPort<?>>();
+
         for (ChannelPortsRequest<?> request : channelRequests) {
             final String channelId = request.getChannelId();
             final PortType channelPortType = request.getPortType();
             final Class<?> channelType = request.getType();
-            SchedulerChannel<?> entry =
-                    this.getSchedulerChannelEntry(channelId, channelType);
-            
+            SchedulerChannel<?> entry = this.getSchedulerChannelEntry(channelId, channelType);
+
             if (channelPortType.IsInput) {
-                inputChannels.put(
-                        channelId,
-                        entry.getInChannel(lightweightProcess, channelPortType));
+                inputChannels.put(channelId, entry.getInChannel(lightweightProcess, channelPortType));
             } else {
-                outputChannels.put(
-                        channelId,
-                        entry.getOutChannel(lightweightProcess, channelPortType));
+                outputChannels.put(channelId, entry.getOutChannel(lightweightProcess, channelPortType));
             }
         }
-        
-        lightweightProcess.initialize(
-                new ChannelPortsCreated(inputChannels, outputChannels));
+
+        lightweightProcess.initialize(new ChannelPortsCreated(inputChannels, outputChannels));
     }
 
     public void performIteration() {
-        Queue<LightweightProcess> queue =
-                new LinkedList<LightweightProcess>(this.processes);
-        Queue<LightweightProcess> queue2 =
-                new LinkedList<LightweightProcess>();
+        List<SimpleEntry<LightweightProcess, SubIterationRequest>> data = new LinkedList<SimpleEntry<LightweightProcess, SubIterationRequest>>();
+
         // make sure all channels are built
         for (SchedulerChannel<?> entry : this.channels.values()) {
             entry.build();
         }
-        
-        boolean allRemainingProcessesAreWaiting = true;
-        
-        // execute all preIteration methods
+
+        // initialize
         for (LightweightProcess process : this.processes) {
+            data.add(new SimpleEntry<LightweightProcess, SubIterationRequest>(process, new SubIterationRequest()));
+
+            // and execute the preIteration methods
             process.preIteration();
         }
-        
-        while (!queue.isEmpty()) {
-            // get process
-            LightweightProcess process = queue.poll();
-            
-            // execute it
-            ExecutionState newState = process.execute();
-            
-            if (newState == null) {
-                logger.severe(this.id + " | ExecutionState 'null' of process : " + process);
-                throw new IllegalStateException("returned ExecutionState was null !");
+
+        // repeat until the iteration has ended
+        while (!data.isEmpty()) {
+            // execute the LightweightProcesses
+            for (int index = 0; index < data.size(); index++) {
+                SimpleEntry<LightweightProcess, SubIterationRequest> element = data.get(index);
+                LightweightProcess process = element.getKey();
+                SubIterationRequest request = element.getValue();
+
+                // reset the request
+                request.reset();
+
+                // execute it
+                process.execute(request);
             }
-            
-            // if the state is not finished execute it again !
-            if (newState != ExecutionState.Finished) {
-                // add it at the end of the queue
-                queue2.add(process);
+
+            // forward stuff immediately... implement something better later !
+            for (SchedulerChannel<?> channel : this.channels.values()) {
+                channel.forwardMessages();
             }
-            
-            allRemainingProcessesAreWaiting =
-                    allRemainingProcessesAreWaiting
-                    && (newState == ExecutionState.Waiting);
-            
-            if (queue.isEmpty()) {                
-                if (allRemainingProcessesAreWaiting) {
-                    // all remaining processes are waiting - the iteration has 
-                    // therefore finished !
-                    break;
+
+            // now check which LightWeightProcesses should be executed again
+            int deleted = 0;
+
+            for (int index = 0; index < data.size(); index++) {
+                SimpleEntry<LightweightProcess, SubIterationRequest> element = data.get(index);
+                SubIterationRequest request = element.getValue();
+
+                if (!request.executeInNextSubIteration()) {
+                    data.remove(index - deleted);
+                    deleted++;
                 }
-                
-                // forward stuff immediately... implement something better later !
-                for (SchedulerChannel<?> channel : this.channels.values()) {
-                    channel.forwardMessages();
-                }
-                
-                // change the channels
-                Queue<LightweightProcess> temp = queue;
-                
-                queue = queue2;
-                queue2 = temp;
-                
-                // reset the variable that determines whether all proceses
-                // are waiting (and therefore the iteration should finish)
-                allRemainingProcessesAreWaiting = true;
             }
         }
-        
+
         // execute all postIteration methods
         for (LightweightProcess process : this.processes) {
             process.postIteration();
--- src/net/matthiasauer/stwp4j/SubIterationRequest.java
+++ src/net/matthiasauer/stwp4j/SubIterationRequest.java
@@ -1,6 +1,17 @@
 package net.matthiasauer.stwp4j;
 
 public class SubIterationRequest {
+    SubIterationRequest() {
+    }
+    
     public void triggerIfNotEmpty(ChannelInPort<?> channelInPort) {
+    }
+    
+    void reset() {
+    }
+
+    boolean executeInNextSubIteration() {
+        // TODO Auto-generated method stub
+        return false;
     }
 }
